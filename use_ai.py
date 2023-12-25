a73c4273a5f77c02171276bccf8c0ddd39282177
DPamK <641439534@qq.com>
2023-12-26 00:57:49 +0800

    正在构qianfanAgent

--- use_ai.py
+++ use_ai.py
@@ -8,13 +8,31 @@ class AgentLeader():
         self.dia_analyizer = 
 
 
-class AiAgent():
-    def __init__(self,api_key,secret_key) -> None:
-
-        self.access_token = self.get_access_token(api_key,secret_key)
+class Agent():
+    def __init__(self,client_id, client_secret) -> None:
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.access_token = self.get_access_token()
     
-    def get_access_token(self,api_key,secret_key):
-        url = f"https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={api_key}&client_secret={secret_key}"
+    def get_access_token(self):
+        url = 'https://example.com/oauth/token'
+        payload = {
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'grant_type': 'client_credentials'
+        }
+        response = requests.post(url, data=payload)
+        response.raise_for_status()
+        return response.json()['access_token']
+    
+
+class QianfanAgent(Agent):
+    def __init__(self, client_id, client_secret,api_url) -> None:
+        super().__init__(client_id, client_secret)
+        self.api_url = api_url
+    
+    def get_access_token(self):
+        url = f"https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={self.client_id}&client_secret={self.client_secret}"
     
         payload = json.dumps("")
         headers = {
@@ -24,3 +42,12 @@ class AiAgent():
         
         response = requests.request("POST", url, headers=headers, data=payload)
         return response.json().get("access_token")
+
+    def request_api(self,msg):
+        payload = json.dumps(msg)
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        response = requests.request("POST",self.api_url, headers=headers, data=payload)
+        return response
+
